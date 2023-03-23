4c3bf12f28e766a3583d363b3890a6f31018765b
BSTNVLDS <bastianvaldesadasme@gmail.com>
2023-03-23 15:58:45 -0300

    add actions

--- login/src/main/java/com/accenture/login/view/LoginFragment.kt
+++ login/src/main/java/com/accenture/login/view/LoginFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.accenture.base.extensions.bottomSheet
+import com.accenture.login.INFO_IN_WORKING
+import com.accenture.login.INFO_UNAVAILABLE
+import com.accenture.login.R
 import com.accenture.login.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -17,5 +22,14 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.singUp.setOnClickListener {
+            childFragmentManager.bottomSheet(INFO_UNAVAILABLE, INFO_IN_WORKING)
+        }
+        binding.singIn.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_form)
+        }
+        binding.resetPassword.setOnClickListener {
+            childFragmentManager.bottomSheet(INFO_UNAVAILABLE, INFO_IN_WORKING)
+        }
     }
 }
